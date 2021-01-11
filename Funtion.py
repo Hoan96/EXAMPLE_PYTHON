902e9c14715fce2fbd9b6d0129a2d4995319b686
Hoan96 <boyf79497@gmail.com>
2021-01-11 22:27:40 +0700

    Update Funtion.py
    Class Python

--- Funtion.py
+++ Funtion.py
@@ -2,3 +2,14 @@ def my_funtion(*kids):
 	print("Hello  from a funtion"+kids[2])
 
 my_funtion("NAME","HHI","ÁDSD")
+
+# Class
+class Persion():
+	"""docstring for Persion"""
+	def __init__(self, name,age):
+		self.name = name
+		self.age = age
+p1=Persion("Jione",36)
+print(p1.age)
+print(p1.name)
+		
