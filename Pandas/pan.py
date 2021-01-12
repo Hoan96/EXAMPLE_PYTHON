4ef7b4cfc3608d48e653ed8f9903c193c6fe2d05
Hoan96 <boyf79497@gmail.com>
2021-01-12 15:48:19 +0700

    Do work with excel

--- Pandas/pan.py
+++ Pandas/pan.py
@@ -11,6 +11,13 @@ excel = pd.read_csv('fie.csv')
 #print(excel.tail()) #Lấy 5 mảng sau cùng
 #print(excel.head()) #Mặc định lấy 5 mảng đầu
 #new_ = excel.dropna() ## Xóa row mà có ô không có giá trị hoặc bằng null
-excel.fillna(130,inplace=True) ## Điều vào ô không có giá trị bằng giá trị cần move
-
+#excel.fillna(130,inplace=True) ## Điều vào ô không có giá trị bằng giá trị cần move
+#excel['DATE'] = pd.to_datetime(['DATE'])  convert to datetime
+#excel.dropna(subset=['DATE'],inplace=True) #Xóa row không có giá trị
+#excel.loc[7,'STT'] = 45 # Gán giá trị mưới vào sô trong row số 7 ô STT
+#for x in excel.index:
+#	if excel.loc[x,'STT']>9:
+#		excel.drop(x,inplace=True) #Xóa row mong muốn
+#excel.drop(7,inplace=True)
+excel.drop_duplicates(inplace = True) #Xóa 2 row trung nhau
 print(excel.to_string())
