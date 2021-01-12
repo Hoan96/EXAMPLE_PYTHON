94f29877151cfe06821938e05aaf4a7c9ea81108
Hoan96 <boyf79497@gmail.com>
2021-01-12 21:44:32 +0700

    Update mat.py

--- Matplotlib/mat.py
+++ Matplotlib/mat.py
@@ -2,8 +2,12 @@ import matplotlib.pyplot as plt
 
 import numpy as np 
 
-
-xpoint = np.array([0,30,60,0,60,0])
-ypoint = np.array([0,500,0,300,300,0])
-plt.plot(xpoint, ypoint,'*:r',ms = 20)
+x= [0,30,60,0,60,0]
+y = [0,500,0,300,300,0]
+for z in range(20):
+	x.append(z*3)
+	y.append(300)
+xpoint = np.array(x)
+ypoint = np.array(y)
+plt.plot(xpoint, ypoint,'o:r',ms = 20)
 plt.show()
