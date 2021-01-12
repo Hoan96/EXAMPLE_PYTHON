58f9c762df606c6a825a80ec9beaba2900787c19
Hoan96 <boyf79497@gmail.com>
2021-01-12 10:17:46 +0700

    update create tag

--- xml.py
+++ xml.py
@@ -1,4 +1,21 @@
 import xml.etree.ElementTree as ET
+
+class Tag():
+	"""docstring for Persion"""
+	def __init__(self, ids,name,addr,fun,tagvalue):
+		self.ids = ids
+		self.name = name
+		self.addr = addr
+		self.fun = fun
+		self.tagvalue = tagvalue
+
 tree = ET.parse('XXX.xml')
 root = tree.getroot()
-print(root[1].attrib)
+TagMangement = []
+#print(root[1].attrib)
+for elem  in root:
+	#print(elem.attrib['TagID'])
+	tag = Tag(elem.attrib['TagID'],elem.attrib['TagName'],elem.attrib['TagAddress'],elem.attrib['TagFunc'],elem.attrib['TagValue'])
+	TagMangement.append(tag)
+for tagss in TagMangement:
+	print(tagss.name)
